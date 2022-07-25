fdfdd2ffbad80d9bd3f28be48b3c4815fda91978
Reynolds Sharp <sharpdesignco@Reynoldss-iPro.attlocal.net>
2022-07-25 15:08:36 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route, Outlet, NavLink } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects/>} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Route>
+    </Routes>
   );
-}
+};
+
+const Layout = () => {
+  const style = ({ isActive }) => ({
+    fontWeight: isActive ? 'bold' : 'normal',
+  });
+
+  return (
+    <>
+      <h1>React Router</h1>
+
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <NavLink to="/home" style={style}>
+          Home
+        </NavLink>
+        <NavLink to="/about" style={style}>
+          About
+        </NavLink>
+        <NavLink to="/projects" style={style}>
+          Projects
+        </NavLink>
+        <NavLink to="/contact" style={style}>
+          Contact
+        </NavLink>
+      </nav>
+
+      <main style={{ padding: '1rem 0' }}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <h2>Home</h2>
+    </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <h2>About</h2>
+    </>
+  );
+};
+
+const Projects = () => {
+  return (
+    <>
+      <h2>Projects</h2>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <h2>Contact</h2>
+    </>
+  );
+};
 
 export default App;
